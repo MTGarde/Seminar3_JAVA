926001dcd358903c4a03b8259db5f2eb8bdccb9c
KarinaSkirmante <karina.krinkele@gmail.com>
2024-03-14 10:30:34 +0200

    New: User

---
+++ src/model/user/User.java
@@ -0,0 +1,62 @@
+package model.user;
+
+import java.security.MessageDigest;
+
+public abstract class User extends GuestUser {
+	//1.variables
+	private String username;
+	//TODO uztaisit enkodesanu, piemeram ar MD5
+	private String password;
+	private String nameAndSurnameOrTitle;
+	
+	
+	//2. get and set
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername() {
+		this.username = nameAndSurnameOrTitle+ "_" + getUserID();
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		//TODO ievietot enkodēšanu
+		if(password != null && password.matches("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,40}$"))
+			this.password = password;
+		else
+			this.password = "--------";
+	}
+	public String getNameAndSurnameOrTitle() {
+		return nameAndSurnameOrTitle;
+	}
+	public abstract void setNameAndSurnameOrTitle(String nameAndSurnameOrTitle);
+	
+
+
+	//3. constructors
+	public User() {
+		super(); //GuestUser()
+		setPassword("1234567890");
+		
+	}
+	
+	public User(String password) {
+		super();
+		setPassword(password);
+	}
+	
+	
+	//4. toString
+	
+	public String toString() {
+		return super.toString() + ":";
+	}
+	
+	//5. other functions
+	
+	//TODO login()
+	//TODO followPage()
+	
+
+}
